bebc5d2eae02ceffc9467c4dacdf496eac5b799c
Fábio Gomes <fabioxgn@gmail.com>
2014-11-11 14:56:53 -0200

    Adds -stdin option to read messages from the stdin

--- slackbot-console/main.go
+++ slackbot-console/main.go
@@ -1,28 +1,47 @@
 package main
 
 import (
+	"bytes"
+	"flag"
+	"fmt"
 	"github.com/cadena-monde/slackbot"
 	"os"
-	"flag"
+	"strings"
 )
 
 var (
 	channel, message string
-	slackURL        = os.Getenv("SLACKBOT_URL")
-	token           = os.Getenv("SLACKBOT_TOKEN")
+	stdin            bool
+	slackURL         = os.Getenv("SLACKBOT_URL")
+	token            = os.Getenv("SLACKBOT_TOKEN")
 )
 
 func main() {
 	parseFlags()
-	postMessage();
+	if stdin {
+		readFromStdin()
+	}
+	postMessage()
+}
+
+func readFromStdin() {
+	input := bytes.NewBuffer([]byte{})
+	input.ReadFrom(os.Stdin)
+
+	inputString := input.String()
+	if inputString != "" {
+		message = fmt.Sprintf("%s\n```%s```", message, input.String())
+		message = strings.Replace(message, "\r", "", -1)
+	}
 }
 
 func parseFlags() {
 	flag.StringVar(&channel, "channel", "", "Channel. Ex: #random")
 	flag.StringVar(&message, "message", "", "Message to be sent to the channel")
+	flag.BoolVar(&stdin, "stdin", false, "Read message from stdin")
 	flag.Parse()
 
-	if channel == "" || message == "" {
+	if channel == "" || (message == "" && !stdin) {
 		flag.Usage()
 		os.Exit(1)
 	}
